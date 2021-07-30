b9b112c38202f35ef896038fcbab9b946d5c8ca3
Juan <jb@hotmail.com>
2021-07-29 22:32:58 -0300

    creo componentes

---
+++ src/MovieCard.jsx
@@ -0,0 +1,9 @@
+export function MovieCard({ movie }) {
+  const imageUrl = `https://image.tmdb.org/t/p/w300${movie.poster_path}`;
+  return (
+    <li>
+      <img src={imageUrl} alt={movie.title} />
+      {movie.title}
+    </li>
+  );
+}
---
+++ src/MoviesGrid.jsx
@@ -0,0 +1,13 @@
+import { MovieCard } from './MovieCard';
+import movies from './movies.json';
+
+export function MoviesGrid() {
+  console.log(movies);
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
+}
