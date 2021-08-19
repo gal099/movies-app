7388dd0af051116b91cd3c0f993f3c52b3048a63
Juan <jb@hotmail.com>
2021-08-19 18:00:08 -0300

    agrego llamado a la api

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,7 +1,27 @@
-import movie from './movie.json';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { Spinner } from '../components/Spinner';
+import { get } from '../utils/httpClient';
 import styles from './MovieDetails.module.css';
 
 export function MovieDetails() {
+  const { movieId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    get('/movie/' + movieId).then((data) => {
+      setMovie(data);
+      setIsLoading(false);
+    });
+  }, [movieId]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
   return (
     <div className={styles.detailsContainer}>
