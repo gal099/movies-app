aa859d0fb97b6a2ba2a82f74b517f88c3109849c
Juan <jb@hotmail.com>
2021-08-24 18:20:56 -0300

    agrego search

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useQuery } from '../hooks/useQuery';
 import { get } from '../utils/httpClient';
 import { MovieCard } from './MovieCard';
 import styles from './MoviesGride.module.css';
@@ -8,13 +9,19 @@ export function MoviesGrid() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setisLoading] = useState(true);
 
+  const query = useQuery();
+  const search = query.get('search');
+
   useEffect(() => {
     setisLoading(true);
-    get('/discover/movie').then((data) => {
+    const searchUrl = search
+      ? '/search/movie?query=' + search
+      : '/discover/movie';
+    get(searchUrl).then((data) => {
       setMovies(data.results);
       setisLoading(false);
     });
-  }, []);
+  }, [search]);
 
   if (isLoading) {
     return <Spinner />;
